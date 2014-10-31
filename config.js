933f85b71d76c5d3b09b6f11b015443b0e4572d9
nkot <nazar.kotylo@gmail.com>
2014-10-31 11:36:05 +0200

    service bus sample

---
+++ config.js
@@ -0,0 +1,3 @@
+﻿module.exports = {
+    "sbConnection" : "Endpoint=sb://kotylowesteurope.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=R2wMqoH0/ohBunJ6MY7SIFBvEfYEPmpySgKSoiicVSE="
+};
--- server.js
+++ server.js
@@ -1,5 +1,55 @@
-var http = require('http')
+//Sample code for Service Bus Queues
+//Update the connecting string for Service Bus on the config.js
+var azure = require('azure');
+var config = require('./config');
+var http = require('http');
+var queue = 'ordersqueue';
+
+var serviceBusClient = azure.createServiceBusService(config.sbConnection);
+
+function createQueue() {
+    serviceBusClient.createQueueIfNotExists(queue, function (error) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log('Queue ' + queue + ' exists');
+        }
+    });
+}
+
+function sendMessage(message) {
+    serviceBusClient.sendQueueMessage(queue, message, function (error) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log('Message sent to queue');
+        }
+    });
+}
+function receiveMessages() {
+    serviceBusClient.receiveQueueMessage(queue, function (error, message) {
+        if (error) {
+            console.log(error);
+        } else {
+            var message = JSON.parse(message.body);
+            console.log('Processing Order# ' + message.OrderId 
+                + ' placed on ' + message.OrderDate);
+        }
+    });
+}
+
+createQueue();
+
+var orderMessage = { "OrderId": 101, "OrderDate": new Date().toDateString() };
+sendMessage(JSON.stringify(orderMessage));
+
+receiveMessages();
+
+
 var port = process.env.PORT || 1337;
+
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Azure Node.js World!\n');
